d824add737844e08832f123933e2816f8f0655f8
Mich3l14 <theo.subud@gmail.com>
2019-10-28 01:03:28 +0100

    bounding box support

--- include/moving_sphere.h
+++ include/moving_sphere.h
@@ -1,7 +1,6 @@
 #ifndef MOVINGSPHEREH
 #define MOVINGSPHEREH
 
-
 #include "hitable.h"
 
 class moving_sphere : public hitable{
@@ -10,6 +9,7 @@ public:
     moving_sphere(vec3 cen0, vec3 cen1,float t0, float t1, float r, material *m) : 
     center0(cen0), center1(cen1), time0(t0), time1(t1) , radius(r), mat_ptr(m){};
     virtual bool hit(const ray &r, float t_min, float t_max, hit_record &rec) const;
+    virtual bool bounding_box(float t0, float t1, aabb& box) const;
     vec3 center(float time) const;
     vec3 center0, center1;
     float time0, time1;
@@ -17,8 +17,8 @@ public:
     material *mat_ptr;
 };
 
-vec3 moving_sphere::center(float time) const{
-    return center0 + (center1-center0)*((time - time0)/(time1 - time0));
+vec3 moving_sphere::center(float time) const {
+    return (center1-center0)*((time - time0)/(time1 - time0)) + center0;
 }
 
 bool moving_sphere::hit(const ray &r, float t_min, float t_max, hit_record &rec) const
@@ -51,6 +51,13 @@ bool moving_sphere::hit(const ray &r, float t_min, float t_max, hit_record &rec)
         }
     }
     return false;
-};
+}
+
+bool moving_sphere::bounding_box(float t0, float t1, aabb& box) const {
+        aabb box0(center(t0) - vec3(radius, radius, radius), center(t0) + vec3(radius, radius, radius));
+        aabb box1(center(t1) - vec3(radius, radius, radius), center(t1) + vec3(radius, radius, radius));
+        box = surrounding_box(box0, box1);
+        return true;
+}
 
 #endif
